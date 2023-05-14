4591277e9ec843dac7ab62a5895f7f8726e03abc
Suhaib Saeed <saeed.suhaib@googlemail.com>
2023-05-14 02:31:55 +0100

    Added interface, ospf and bgp tests

--- network-testing-with-pytest/tests/test_ios.py
+++ network-testing-with-pytest/tests/test_ios.py
@@ -57,7 +57,7 @@ def get_interface_output(task):
     # Use nornir to send command to device
     result = task.run(
         task=netmiko_send_command,
-        command_string="show ip interface brief",
+        command_string="show ip interface brief | exclude unassigned",
         use_textfsm=True,
     )
     # Store result in nornir host object
@@ -143,7 +143,30 @@ class TestInterfaces:
         # Filter on ios devices
         ios_devices = pytestnr.filter(F(groups__contains="ios"))
         
-        # Run get_ospf_output and get_bgp_output tasks
-        tasks = [get_ospf_output, get_bgp_output]
+        # Run get_interface_output task
+        tasks = [get_interface_output]
         for atask in tasks:
             ios_devices.run(task=atask)
+    
+    @pytest.mark.parametrize("device_name", get_ios_device_names())
+    def test_interface_status(self, pytestnr, device_name):
+
+        """Test to check for 2 interfaces in up/up state"""
+
+        # Get nornir host object for device
+        nr_host = pytestnr.inventory.hosts[device_name]
+        # Get interface data from nornir host object
+        interfaces = nr_host["interface_output"]
+
+        down_interfaces = []
+
+        # Loop through interface data and count interfaces in up/up state
+        for interface in interfaces:
+            if interface['status'] != 'up':
+                down_interfaces.append(interface['intf'])
+
+        num_down_interfaces = len(down_interfaces)
+        # Assert expected and actual interfaces are the same
+        assert (
+            num_down_interfaces == 0
+        ), f"{device_name} has {num_down_interfaces} interfaces not in the up state"
